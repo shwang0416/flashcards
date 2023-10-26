f22dab09af82d1537cec82e29f3ba8bc655e53fc
Hwang Sujung <crystalhwang0416@gmail.com>
2023-10-26 23:50:07 +0900

    fix: add "use server" in the top of the server action file

--- src/adaptor/serverActions/getCardByIdAction.ts
+++ src/adaptor/serverActions/getCardByIdAction.ts
@@ -1,11 +1,9 @@
+"use server";
+
 import notion from "@/lib/notionhq";
 
 const getCardById = async (pageId: string): Promise<any> => {
-  // const DATABASE_ID = process.env.NOTION_DATABASE_ID;
-  // if (!DATABASE_ID) throw new Error("notion env is missing!!!");
-
   return await notion.pages.retrieve({ page_id: pageId });
-  // console.log(JSON.stringify(query));
 };
 
 export default getCardById;
