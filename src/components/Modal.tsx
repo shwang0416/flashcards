5f7f2f8c9846df5e1a4d558a2ef9c433e0181d71
Hwang Sujung <crystalhwang0416@gmail.com>
2023-11-05 23:05:37 +0900

    feat: add default button callback to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export interface ModalContents {
   title: string;
   description: string[];
   buttonText: string;
 }
 
-type ModalProps = ModalContents & { buttonCallback: () => void };
+type ModalProps = ModalContents & { buttonCallback?: () => void };
 
 const Modal = ({
   title,
@@ -12,15 +16,21 @@ const Modal = ({
   buttonText,
   buttonCallback,
 }: ModalProps) => {
+  const router = useRouter();
+  if (!buttonCallback) {
+    buttonCallback = router.back;
+  }
   return (
-    <div className="absolute flex flex-col gap-y-10 items-center bg-slate-100 rounded-xl shadow-lg shadow-pink-200 top-[50%] left-0 right-0 ml-auto mr-auto min-w-[500px] w-fit h-fit p-10 ">
-      <h4 className="text-xl font-semibold">{title}</h4>
-      <div className="flex flex-col items-center">
-        {description.map((elem: string) => (
-          <span key={elem}>{elem}</span>
-        ))}
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="absolute flex flex-col gap-y-10 items-center bg-slate-100 rounded-xl shadow-lg shadow-pink-200 top-[50%] left-0 right-0 ml-auto mr-auto min-w-[500px] w-fit h-fit p-10 ">
+        <h4 className="text-xl font-semibold">{title}</h4>
+        <div className="flex flex-col items-center">
+          {description.map((elem: string) => (
+            <span key={elem}>{elem}</span>
+          ))}
+        </div>
+        <button onClick={buttonCallback}>{buttonText}</button>
       </div>
-      <button onClick={buttonCallback}>{buttonText}</button>
     </div>
   );
 };
