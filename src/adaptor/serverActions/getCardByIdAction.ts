d80dcf680185931b20871066c9312b7f4b570be1
Hwang Sujung <crystalhwang0416@gmail.com>
2023-11-07 00:31:28 +0900

    remove: unused controllers for notion database

--- src/adaptor/serverActions/getCardByIdAction.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import notion from "@/lib/notionhq";
-
-const getCardById = async (pageId: string): Promise<any> => {
-  return await notion.pages.retrieve({ page_id: pageId });
-};
-
-export default getCardById;
--- src/adaptor/serverActions/getRecordMapByIdAction.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import notion from "@/lib/notion-client";
-
-const getRecordMapById = async (id: string) => {
-  return await notion.getPage(id);
-};
-
-export default getRecordMapById;
