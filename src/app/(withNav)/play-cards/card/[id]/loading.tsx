59b00d4ede2c15989b8fefeaac4ee6ba082a930b
Hwang Sujung <crystalhwang0416@gmail.com>
2023-11-09 22:50:08 +0900

    feat: add loading.tsx

---
+++ src/app/(withNav)/play-cards/card/[id]/loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div className="">Card Loading ...</div>;
+};
+
+export default Loading;
---
+++ src/app/(withNav)/play-cards/loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <div className="">Page Loading ...</div>;
+};
+
+export default Loading;
