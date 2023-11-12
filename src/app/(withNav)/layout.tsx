a8da911e2057248577075fb7f4e02a37cf1baa29
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-12 20:44:38 +0900

    feat: update nav bar ui

--- src/app/(withNav)/layout.tsx
+++ src/app/(withNav)/layout.tsx
@@ -19,26 +19,26 @@ export default function RootLayout({
       <div className="flex justify-between bg-slate-600 p-2 rounded-xl ">
         <Link
           href="/daily-check-in"
-          className="text-white hover:bg-pink-300 p-2 rounded-lg"
+          className="text-white border border-transparent hover:bg-slate-500 hover:border-slate-400 p-2 rounded-lg"
         >
           <span className="mr-1 inline">Daily Check in</span>
           <CalendarDaysIcon className="h-6 w-6 text-white inline" />
         </Link>
         <Link
           href="/play-cards"
-          className="text-white hover:bg-pink-300 p-2 rounded-lg"
+          className="text-white border border-transparent hover:bg-slate-500 hover:border-slate-400 p-2 rounded-lg"
         >
           <span className="mr-1 inline">Play Cards</span>
           <PlayIcon className="h-6 w-6 text-white inline" />
         </Link>
         <Link
           href="/cards-box"
-          className="text-white hover:bg-pink-300 p-2 rounded-lg"
+          className="text-white border border-transparent hover:bg-slate-500 hover:border-slate-400 p-2 rounded-lg"
         >
           <span className="mr-1 inline">Cards Box</span>
           <ArchiveBoxIcon className="h-6 w-6 text-white inline" />
         </Link>
-        <div className="flex gap-x-1 text-white hover:bg-pink-300 p-2 rounded-lg flex-row items-center">
+        <div className="flex gap-x-1 flex-row text-white border border-transparent hover:bg-slate-500 hover:border-slate-400 p-2 rounded-lg items-center">
           <LogoutForm />
           <ArrowRightOnRectangleIcon className="h-6 w-6 text-white inline" />
         </div>
