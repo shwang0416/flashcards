90033bf7af1cc6b89b2d25db2377d03d5e99785f
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-14 22:09:33 +0900

    remove: unused files

--- src/lib/notion-client.ts
+++
@@ -1,5 +0,0 @@
-import { NotionAPI } from "notion-client";
-
-const notion = new NotionAPI();
-
-export default notion;
--- src/lib/notionhq.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "@notionhq/client";
-
-const API_KEY = process.env.NOTION_KEY;
-if (!API_KEY) throw new Error("NOTION_API_KEY is missing");
-
-const notion = new Client({ auth: API_KEY });
-
-export default notion;
