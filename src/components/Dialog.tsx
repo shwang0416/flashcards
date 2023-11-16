fe17b44d7db61e5c0c1601ee69118d0b1c9158bc
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-16 18:16:34 +0900

    feat: update dialog ui

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -12,14 +12,14 @@ const Dialog = ({ children }: { children: ReactNode }) => {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
-      <div className="backdrop-blur-lg absolute flex flex-col items-center bg-slate-100 rounded-xl shadow-xl shadow-pink-200 top-[10%] left-0 right-0 ml-auto mr-auto min-w-[500px] w-[80%] h-[80%] p-6 ">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="absolute left-0 right-0 top-[10%] ml-auto mr-auto flex h-[80%] w-[80%] min-w-[500px] flex-col items-center rounded-xl bg-slate-100 p-6 shadow-xl shadow-pink-200 backdrop-blur-lg ">
         <button
-          className="p-0 w-full flex justify-end mb-4 bg-transparent hover:bg-transparent"
+          className="mb-4 flex w-full justify-end"
           onClick={backdropClickHandler}
           type="button"
         >
-          <XMarkIcon className="w-6 h-6 text-gray-600 hover:bg-pink-200 hover:text-white rounded cursor-pointer" />
+          <XMarkIcon className="h-6 w-6 cursor-pointer rounded text-gray-600 hover:bg-pink-200 hover:text-white" />
         </button>
         {children}
       </div>
