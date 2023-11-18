983c698bd22f3f218cfbecc6e203ad2d96135229
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-18 13:19:01 +0900

    feat: add error.tsx

---
+++ src/app/(withNav)/play-cards/card/[id]/front/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+
+const Error = () => (
+    <div className="">
+      <h2>에러 발생</h2>
+      <p>카드를 찾을 수 없습니다</p>
+      <Link href="/">홈으로</Link>
+    </div>
+  );
+
+export default Error;
