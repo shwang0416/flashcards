722332e89dd4eb37803d63fe19f756a60caa01a8
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-18 17:41:12 +0900

    feat: filter cards in cards-box by tags

---
+++ src/app/(withNav)/cards-box/SelectTagCardContainer.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useMemo, useState } from "react";
+import MultiSelectTagsController from "@/components/MultiSelectTagsController";
+import { TagStatus } from "@/data/Tag";
+import { Card } from "@/entity/Card";
+import {
+  cardsToTags,
+  filterCardsByTags,
+  setAllTagStatus,
+  tagStatusToTags,
+} from "@/entity/util/Tag";
+import CardContainer from "./CardContainer";
+
+type SelectTagCardContainerProps = {
+  cards: Card[];
+  deleteMode: boolean;
+};
+
+const SelectTagCardContainer = ({
+  cards,
+  deleteMode,
+}: SelectTagCardContainerProps) => {
+  const [remoteTags] = useState(cardsToTags(cards));
+  const [tagStatus, setTagStatus] = useState<TagStatus>(
+    setAllTagStatus(remoteTags, false),
+  );
+
+  //   tagStatus로 카드를 필터링해서 넘긴다
+  const filteredCards = useMemo(() => {
+    const selectedTags = tagStatusToTags(tagStatus);
+    return filterCardsByTags(cards, selectedTags);
+  }, [cards, tagStatus]);
+
+  return (
+    <div className="flex h-full flex-col gap-4 pt-4">
+      <MultiSelectTagsController
+        tags={remoteTags}
+        tagStatus={tagStatus}
+        updateTagStatus={setTagStatus}
+      />
+      <CardContainer cards={filteredCards} deleteMode={deleteMode} />
+    </div>
+  );
+};
+
+export default SelectTagCardContainer;
--- src/app/(withNav)/cards-box/page.tsx
+++ src/app/(withNav)/cards-box/page.tsx
@@ -1,18 +1,17 @@
 import getUserAction from "@/adaptor/serverActions/auth/getUserAction";
 import getCardListAction from "@/adaptor/serverActions/getCardListAction";
-import CardContainer from "./CardContainer";
+import SelectTagCardContainer from "./SelectTagCardContainer";
 
 const Page = async ({ searchParams }: { searchParams: { d: boolean } }) => {
+  const { d: deleteMode } = searchParams;
   const user = await getUserAction();
   if (!user) throw new Error("ERROR: no user");
-
-  const { d: deleteMode } = searchParams;
 
   const cards = await getCardListAction({ userId: user.id });
 
   return (
-    <div className="flex flex-col gap-4 pt-4 h-full">
-      <CardContainer cards={cards} deleteMode={deleteMode} />
+    <div className="flex h-full flex-col gap-4 pt-4">
+      <SelectTagCardContainer cards={cards} deleteMode={deleteMode} />
     </div>
   );
 };
