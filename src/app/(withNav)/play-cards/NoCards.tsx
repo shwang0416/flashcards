8b3a77cedbe010acb7532577a3be7293d15d52fa
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-18 13:30:47 +0900

    refactor: remove unnecessary curly braces

--- src/app/(withNav)/play-cards/NoCards.tsx
+++ src/app/(withNav)/play-cards/NoCards.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
 
-const NoCards = () => {
-  return (
-    <div className="flex h-full flex-grow flex-col  gap-4 pt-4">
-      <div className="flex flex-col items-center rounded-xl  bg-white p-4">
-        <h4 className="h-24 text-4xl font-semibold underline decoration-slate-200 decoration-double">
-          카드가 없습니다
-        </h4>
-        <Link href="/play-cards" className="button-default">
-          돌아가기
-        </Link>
-      </div>
+const NoCards = () => (
+  <div className="flex h-full flex-grow flex-col  gap-4 pt-4">
+    <div className="flex flex-col items-center rounded-xl  bg-white p-4">
+      <h4 className="h-24 text-4xl font-semibold underline decoration-slate-200 decoration-double">
+        카드가 없습니다
+      </h4>
+      <Link href="/play-cards" className="button-default">
+        돌아가기
+      </Link>
     </div>
-  );
-};
+  </div>
+);
 
 export default NoCards;
