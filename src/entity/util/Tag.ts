3ee582bbbbda1d102997b211e6140deb31b7af64
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-18 17:31:16 +0900

    feat: add util functions to manipulate tag data

---
+++ src/entity/util/Tag.ts
@@ -0,0 +1,21 @@
+import { TagStatus } from "@/data/Tag";
+import { Card } from "../Card";
+
+export const tagStatusToTags = (src: TagStatus) =>
+  Object.keys(src).filter((tag) => !!src[tag]);
+
+export const cardsToTags = (src: { tags: string[] }[]) => {
+  const tagSet = src.reduce((acc, { tags }) => {
+    if (tags && tags.length > 0) tags.forEach((t: string) => acc.add(t));
+    return acc;
+  }, new Set<string>());
+
+  return [...tagSet];
+};
+
+export const filterCardsByTags = (cards: Card[], tags: string[]) =>
+  cards.filter((card) =>
+    tags.some(
+      (tag) => card.tags && card.tags.length > 0 && card.tags.includes(tag),
+    ),
+  );
