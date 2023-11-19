61d07548b552985df8b1095a9f45457655d82308
Sujeong Artlab <crystalhwang0416@gmail.com>
2023-11-19 23:55:22 +0900

    feat: update loading ui in card modal

--- src/app/(withNav)/cards-box/@modal/(.)card/loading.tsx
+++ src/app/(withNav)/cards-box/@modal/(.)card/loading.tsx
@@ -4,13 +4,14 @@ const Loading = () => (
     className="mt-4 flex h-full w-full animate-pulse items-center justify-center "
   >
     <div className="flex h-full w-full flex-col gap-2">
-      <div className="h-20" />
-      <div className="h-[40px] rounded-xl bg-gray-50 dark:bg-pink-200" />
+      <div className="h-12" />
+      <div className="h-[40px] rounded-xl bg-gray-50 " />
       <div className="flex flex-grow flex-col gap-2 text-xl">
-        <div className="flex h-1/2  w-full flex-col-reverse gap-2 rounded-xl bg-gray-50 dark:bg-pink-200" />
-        <div className="flex h-1/2  w-full flex-col-reverse gap-2 rounded-xl bg-gray-50 dark:bg-pink-200" />
+        <div className="flex h-full  w-full flex-col-reverse gap-2 rounded-xl bg-gray-50 " />
       </div>
-      <div className="h-16 rounded-xl bg-gray-200" />
+      <div className="rounded-xl bg-gray-200 px-6 py-3 text-[24px] text-gray-200">
+        ...
+      </div>
     </div>
     <span className="sr-only">Loading...</span>
   </div>
